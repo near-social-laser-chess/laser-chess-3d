2cd77377d6fec0ca07ce6b30f663a69eabac546d
HolidayMan <lehafirstchanel@gmail.com>
2023-02-18 20:38:26 +0100

    implemented generating build index.html and copying assets to build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path')
+
 module.exports = {
     watch: true,
     entry: "./index.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'build'),
-        publicPath: "/"
+        publicPath: "/",
+        assetModuleFilename: "assets/[name][ext]",
     },
     devServer: {
         port: 8080,
@@ -20,4 +24,14 @@ module.exports = {
     experiments: {
         topLevelAwait: true
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "assets", to: "assets" }
+            ],
+        })
+    ],
 };
