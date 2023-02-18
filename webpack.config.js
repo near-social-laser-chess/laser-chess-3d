ae8bb0cb13cad09877294cdf7a49d4b474d8ce16
HolidayMan <lehafirstchanel@gmail.com>
2023-02-18 21:50:41 +0100

    added exiting from webpack process when build is finished

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,48 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path')
 
-module.exports = {
-    watch: true,
-    entry: "./index.js",
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, 'build'),
-        publicPath: "/",
-        assetModuleFilename: "assets/[name][ext]",
-    },
-    devServer: {
-        port: 8080,
-        historyApiFallback: true,
-        hot: true,
-        client: {
-            overlay: {
-                errors: true,
+module.exports = (env, argv) => {
+    return {
+        watch: true,
+        entry: "./index.js",
+        output: {
+            filename: "bundle.js",
+            path: path.resolve(__dirname, 'build'),
+            publicPath: `${process.env.BASE_URL ? process.env.BASE_URL: ""}/`,
+            assetModuleFilename: "assets/[name][ext]",
+        },
+        devServer: {
+            port: 8080,
+            historyApiFallback: true,
+            hot: true,
+            client: {
+                overlay: {
+                    errors: true,
+                },
             },
         },
-    },
-    experiments: {
-        topLevelAwait: true
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: "assets", to: "assets" }
-            ],
-        })
-    ],
-};
+        experiments: {
+            topLevelAwait: true
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 'index.html',
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {from: "assets", to: "assets"}
+                ],
+            }),
+            {
+                apply: (compiler) => {
+                    compiler.hooks.done.tap('DonePlugin', (stats) => {
+                        if (argv.env.WEBPACK_SERVE) return;
+                        setTimeout(() => {
+                            process.exit(0)
+                        })
+                    });
+                }
+            }
+        ],
+    };
+}
