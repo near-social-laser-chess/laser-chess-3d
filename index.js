a30db2c0d6e7819df3a932b43d62480c4beb6d9a
HolidayMan <lehafirstchanel@gmail.com>
2023-02-18 19:52:36 +0100

    removed unnecessary path objects from laserPath

--- index.js
+++ index.js
@@ -12,22 +12,10 @@ let index = 0;
 const laserPath = [
     {
         startCell: board.findCell(7, 9),
-        endCell: board.findCell(6, 9)
-    },
-    {
-        startCell: board.findCell(6, 9),
-        endCell: board.findCell(5, 9)
-    },
-    {
-        startCell: board.findCell(5, 9),
         endCell: board.findCell(4, 9)
     },
     {
         startCell: board.findCell(4, 9),
-        endCell: board.findCell(4, 8)
-    },
-    {
-        startCell: board.findCell(4, 8),
         endCell: board.findCell(4, 7)
     },
     {
@@ -36,22 +24,10 @@ const laserPath = [
     },
     {
         startCell: board.findCell(3, 7),
-        endCell: board.findCell(3, 8)
-    },
-    {
-        startCell: board.findCell(3, 8),
         endCell: board.findCell(3, 9)
     },
     {
         startCell: board.findCell(3, 9),
-        endCell: board.findCell(2, 9)
-    },
-    {
-        startCell: board.findCell(2, 9),
-        endCell: board.findCell(1, 9)
-    },
-    {
-        startCell: board.findCell(1, 9),
         endCell: board.findCell(0, 9)
     }
 ]
