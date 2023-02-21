104d5fc031a71cef1e6d9c348e8dcbf75b173091
advanced-user <stolbetskiy.yura@gmail.com>
2023-02-22 00:17:52 +0200

    Added the ability to choose a side

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ import {camera, renderer, rotateButtonClockwise,
     rotateButtonCounterClockwise, disableRotateButtons} from "./ui/scene.js";
 import {board} from "./ui/scene.js";
 import {calculateClickedPoint} from "./controller/utils.js";
-import {GameController} from "./controller/main.js";
-import {MovementTypesEnum} from "./game_logic/models/Enums";
+import {MovementTypesEnum, PlayerTypesEnum} from "./game_logic/models/Enums";
 import {initUI} from "./ui/main";
+import {AIGameController} from "./controller/AIGameController";
+import {OnlineGameController} from "./controller/OnlineGameController";
 
 export let gameController;
 
 export const initGame = async () => {
     await initUI();
-    gameController = new GameController();
+    gameController = new AIGameController(PlayerTypesEnum.BLUE, PlayerTypesEnum.RED);
 
     document.addEventListener( 'mouseup', async (e) => {
         const point = calculateClickedPoint(e);
         if (point) {
             const cell = board.getCellByCoords(...point.uv);
             await gameController.clickOnBoard(cell)
-        } else {
+        }
+        /*
+        else {
             gameController.unselectPiece();
         }
+
+         */
     });
 
     document.addEventListener('mousemove', e => {
@@ -35,7 +40,6 @@ export const initGame = async () => {
         camera.updateProjectionMatrix();
         renderer.setSize( window.innerWidth, window.innerHeight );
     });
-
 
     rotateButtonCounterClockwise.addEventListener('click', async () => {
         await gameController.rotatePiece(MovementTypesEnum.ROTATION_C_CLOCKWISE);
